c9ebfc8f059363a856dde8a56f6ce5280ab30747
Brett Kessler <brettkessler1@gmail.com>
2019-08-13 12:03:06 -0500

    test

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.route('/api/games').get((req, res) => {
       console.log(games);
       return res.status(200).json({
         message: 'Games fetched successfully',
-        games: games
+        games: JSON.parse(games)
       });
     })
 })
